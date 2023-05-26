910af4ad2ca85f8b2b74e467b364917fd2319dc2
Tra-My0104 <tramyminxu@gmail.com>
2023-05-26 19:18:53 +0700

    add

--- task-keeper/server/server.js
+++ task-keeper/server/server.js
@@ -11,7 +11,6 @@ server.use(bodyparser.urlencoded({ extended: true }));
 server.use(morgan("dev"));
 server.use(cors());
 
-
 server.get("/api/v1/task", (req, res) => {
   const query = "select * from task";
   database.query(query, (err, result) => {
@@ -30,7 +29,8 @@ server.get("/api/v1/task", (req, res) => {
 server.post("/api/v1/task", (req, res) => {
   const { Content, Duedate, Statustask, Assigned } = req.body;
   const value = [Content, Duedate, Statustask, Assigned];
-  const query = "insert into task(Content, Duedate, Statustask, Assigned) values (?,?,?,?)";
+  const query =
+    "insert into task(Content, Duedate, Statustask, Assigned) values (?,?,?,?)";
   database.query(query, value, (err, result) => {
     console.log(value);
     if (err) {
@@ -45,48 +45,47 @@ server.post("/api/v1/task", (req, res) => {
   });
 });
 
+server.put("/api/v1/task/:id", (req, res) => {
+  const { id } = req.params;
+  const { Content, Duedate, Statustask, Assigned } = req.body;
+  const values = [Content, Duedate, Statustask, Assigned, id];
+  const query =
+    "UPDATE task SET Content=?, Duedate=?, Statustask=?, Assigned=? WHERE TaskId=?";
 
-server.put("/api/v1/task/:id" , (req,res) => {
-    const {id} = req.params;
-    const { Content, Duedate, Statustask, Assigned } = req.body;
-    const values = [Content, Duedate, Statustask, Assigned, id];
-    const query = 'UPDATE task SET Content=?, Duedate=?, Statustask=?, Assigned=? WHERE TaskId=?';
-    
-    database.query(query, values, (err, result) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({
-          status: "error",
-          message: "Có lỗi xảy ra khi cập nhật task",
-          data: null
-        });
-      }
-      
-      return res.status(200).json({
-        status: "success",
-        message: "Cập nhật task thành công",
-        data: result,
+  database.query(query, values, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        status: "error",
+        message: "Có lỗi xảy ra khi cập nhật task",
+        data: null,
       });
+    }
+
+    return res.status(200).json({
+      status: "success",
+      message: "Cập nhật task thành côn",
+      data: result,
     });
   });
-
+});
 
 server.delete("/api/v1/task/:id", (req, res) => {
-    const { id } = req.params;
-    const query = "DELETE FROM task WHERE TaskId=?";
-    database.query(query, id, (err, result) => {
-      if (err) {
-        console.log("kết nối thất bại");
-        res.status(500);
-      } else {
-        return res.status(200).json({
-          status: "success",
-          message: "Xóa thành công",
-          data: result,
-        });
-      }
-    });
+  const { id } = req.params;
+  const query = "DELETE FROM task WHERE TaskId=?";
+  database.query(query, id, (err, result) => {
+    if (err) {
+      console.log("kết nối thất bại");
+      res.status(500);
+    } else {
+      return res.status(200).json({
+        status: "success",
+        message: "Xóa thành công",
+        data: result,
+      });
+    }
   });
+});
 
 server.listen(port, () => {
   console.log(`http://localhost:${port}`);
